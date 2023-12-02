fcd2a0d97897b7089d4d79cc8150218add515803
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-02 00:47:30 -0300

    ⚙️ Configurações do XFCE4

--- config/xfce4/xfce4-config.sh
+++ config/xfce4/xfce4-config.sh
@@ -22,16 +22,16 @@ mkdir -p ~/.vnc
 chmod +x /usr/local/bin/vncserver-start
 chmod +x /usr/local/bin/vncserver-stop
 
-echo '#!/bin/bash
-export PULSE_SERVER=127.0.0.1
-LANG=ptBR.UTF-8
-export LANG
-[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-echo $$ > /tmp/xsession.pid
-dbus-launch --exit-with-session /usr/bin/startxfce4' > ~/.vnc/xstartup
+#echo '#!/bin/bash
+#export PULSE_SERVER=127.0.0.1
+#LANG=ptBR.UTF-8
+#export LANG
+#[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+#[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+#echo $$ > /tmp/xsession.pid
+#dbus-launch --exit-with-session /usr/bin/startxfce4' > ~/.vnc/xstartup
 
-chmod +x ~/.vnc/xstartup
+#chmod +x ~/.vnc/xstartup
 
 echo " "
 
