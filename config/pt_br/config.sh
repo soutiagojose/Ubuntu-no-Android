6edafe8ca2c08cae639cff8b8b23b1bfb6bf70e6
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-02 00:48:53 -0300

    🇧🇷 Definições do idioma Português Brasileiro

--- config/pt_br/config.sh
+++ config/pt_br/config.sh
@@ -8,3 +8,13 @@ echo $$ > /tmp/xsession.pid
 dbus-launch --exit-with-session /usr/bin/startxfce4' > ~/.vnc/xstartup
 
 chmod +x ~/.vnc/xstartup
+
+## Gerar o idioma
+sed -i 's/^# *\(pt_BR.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+## Exportar os comandos de configuração de idioma para ~/.bashrc
+## Essa configuração necessita de reboot
+echo 'export LC_ALL=pt_BR.UTF-8' >> ~/.bashrc
+echo 'export LANG=pt_BR.UTF-8' >> ~/.bashrc
+echo 'export LANGUAGE=pt_BR.UTF-8' >> ~/.bashrc
