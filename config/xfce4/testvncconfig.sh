3fcbd9f7d67153b91152247949c0c4e506f640f0
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-02 02:36:57 -0300

    Delete config/xfce4/testvncconfig.sh

--- config/xfce4/testvncconfig.sh
+++
@@ -1,7 +0,0 @@
-export USER=$(whoami)
-export PORT=1
-GEO="-geometry 1920x1080" vnc
-vncserver -name remote-desktop -geometry 1920x1080 :1
-
-xfce4-panel-profiles load xfce4-panel.tar.bz2
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-image -s  /usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg
