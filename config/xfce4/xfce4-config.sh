c23eb85623e54130c1d911234d04056d3dc5f21f
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-05 18:02:14 -0300

    Update xfce4-config.sh

--- config/xfce4/xfce4-config.sh
+++ config/xfce4/xfce4-config.sh
@@ -22,22 +22,15 @@ export PORT=1
 GEO="-geometry 1920x1080" vnc
 vncserver -name remote-desktop -geometry 1920x1080 :1
 
-# Troca o papel de parede
-echo " "
-echo " "
-echo "Carregando perfil de painel"
-xfce4-panel-profiles load xfce4-panel.tar.bz2
-rm -rf xfce4-panel.tar.bz2
-echo -e '\033[1;36mDefinindo o papel de parede\033[0m'
-echo " "
-echo -e '\033[1;97mPoderá trocar em Aplicativos > Configurações > Área de trabalho > Background\033[0m'
-sed -i 's|backgrounds/xfce/xfce-verticals.png|backgrounds/john-towner-JgOeRuGD_Y4.jpg|' $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
 xfconf-query -c xsettings -p /Net/ThemeName -s ZorinBlue-Dark
 xfconf-query -c xsettings -p /Net/IconThemeName -s Uos-fulldistro-icons
+xfce4-panel-profiles load xfce4-panel.tar.bz2
+sed -i 's|backgrounds/xfce/xfce-verticals.png|backgrounds/john-towner-JgOeRuGD_Y4.jpg|' $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
+
 
 
 vncserver -kill
 rm -rf /tmp/.X$pt-lock
 rm -rf /tmp/.X11-unix/X$pt
-
+rm -rf xfce4-panel.tar.bz2
 exit
