a3057b9ce698e02f46423c2134c86cfebf97e191
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-05 18:13:36 -0300

    Update xfce4-config.sh

--- config/xfce4/xfce4-config.sh
+++ config/xfce4/xfce4-config.sh
@@ -22,10 +22,22 @@ export PORT=1
 GEO="-geometry 1920x1080" vnc
 vncserver -name remote-desktop -geometry 1920x1080 :1
 
-xfconf-query -c xsettings -p /Net/ThemeName -s ZorinBlue-Dark
-xfconf-query -c xsettings -p /Net/IconThemeName -s Uos-fulldistro-icons
-xfce4-panel-profiles load xfce4-panel.tar.bz2
 sed -i 's|backgrounds/xfce/xfce-verticals.png|backgrounds/john-towner-JgOeRuGD_Y4.jpg|' $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
+
+echo "Definindo o tema"
+echo ""
+echo ""
+xfconf-query -c xsettings -p /Net/ThemeName -s ZorinBlue-Dark
+echo "Definindo o pacote de icons"
+echo ""
+echo ""
+xfconf-query -c xsettings -p /Net/IconThemeName -s Uos-fulldistro-icons
+echo "Definindo o painel de menus"
+echo ""
+echo ""
+xfce4-panel-profiles load xfce4-panel.tar.bz2
+echo ""
+echo ""
 
 
 
@@ -33,4 +45,5 @@ vncserver -kill
 rm -rf /tmp/.X$pt-lock
 rm -rf /tmp/.X11-unix/X$pt
 rm -rf xfce4-panel.tar.bz2
+rm -rf Uos-fulldistro-icons.tar.xz
 exit
