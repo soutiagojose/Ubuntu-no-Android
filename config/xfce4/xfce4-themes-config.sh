310948a36345749ffed158cf1bb088eb82c811b8
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-06 01:55:14 -0300

    Update xfce4-themes-config.sh

--- config/xfce4/xfce4-themes-config.sh
+++ config/xfce4/xfce4-themes-config.sh
@@ -5,7 +5,7 @@ wget https://github.com/allytiago/Ubuntu-no-Android/raw/main/config/xfce4/xfce4-
 vncserver -name remote-desktop -geometry 1920x1080 :1
 xfconf-query -c xsettings -p /Net/ThemeName -s ZorinBlue-Dark
 xfconf-query -c xsettings -p /Net/IconThemeName -s Uos-fulldistro-icons
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-image -s john-towner-JgOeRuGD_Y4.jpg
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-image -s /usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg
 xfce4-panel-profiles load xfce4-panel.tar.bz2
 sed -i 's|backgrounds/xfce/xfce-verticals.png|backgrounds/john-towner-JgOeRuGD_Y4.jpg|' ./.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
 xfce4-panel-profiles load xfce4-panel.tar.bz2
