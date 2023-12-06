43572c7ec558b15e1e9c9975e81f16a82dc95279
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-05 22:32:14 -0300

    Update xfce4-themes config.sh

--- config/xfce4/xfce4-themes config.sh
+++ config/xfce4/xfce4-themes config.sh
@@ -6,3 +6,10 @@ xfconf-query -c xsettings -p /Net/IconThemeName -s Uos-fulldistro-icons
 xfce4-panel-profiles load xfce4-panel.tar.bz2
 sed -i 's|backgrounds/xfce/xfce-verticals.png|backgrounds/john-towner-JgOeRuGD_Y4.jpg|' $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
 xfce4-panel-profiles load xfce4-panel.tar.bz2
+
+rm -rf xfce4-panel.tar.bz2
+rm -rf Uos-fulldistro-icons.tar.xz
+
+vncserver -kill
+rm -rf /tmp/.X$pt-lock
+rm -rf /tmp/.X11-unix/X$pt
