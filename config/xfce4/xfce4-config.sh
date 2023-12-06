cf7bfd311c8881cf62733f27d110dab9d685fd8c
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-06 00:09:40 -0300

    Update xfce4-config.sh

--- config/xfce4/xfce4-config.sh
+++ config/xfce4/xfce4-config.sh
@@ -5,3 +5,11 @@ sudo apt-get update
 
 sudo apt-get install xfce4 xfce4-goodies xfce4-terminal xfce4-panel-profiles dbus-x11 --no-install-recommends -y
 sudo apt-get clean
+
+wget --tries=20 https://raw.githubusercontent.com/allytiago/Ubuntu-no-Android/main/config/xfce4/xfce4-themes-config.sh
+chmod +x  xfce4-themes-config.sh
+bash xfce4-themes-config.sh
+
+vncpasswd
+
+exit
