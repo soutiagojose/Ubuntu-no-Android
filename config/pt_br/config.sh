4c755ee9a4504fb86cefe37191e0ad204f06cad8
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-07 20:45:24 -0300

    teste

--- config/pt_br/config.sh
+++ config/pt_br/config.sh
@@ -9,16 +9,24 @@ sudo apt install locales -y
 
 mkdir -p ~/.vnc
 
-echo '#!/bin/bash
+echo "#!/bin/bash
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
 export PULSE_SERVER=127.0.0.1
 LANG=pt_BR.UTF-8
 export LANG
 [ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
 [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-echo $$ > /tmp/xsession.pid
-dbus-launch --exit-with-session /usr/bin/startxfce4' > ~/.vnc/xstartup
+echo $$ > /tmp/xsession.pid" > ~/.vnc/xstartup
+
+# For lxde
+#echo 'dbus-launch --exit-with-session startlxde' > ~/.vnc/xstartup
+
+# For xfce
+echo 'dbus-launch --exit-with-session /usr/bin/startxfce4' > ~/.vnc/xstartup
 
 chmod +x ~/.vnc/xstartup
+
+
 
 ## Gerar o idioma
 sed -i 's/^# *\(pt_BR.UTF-8\)/\1/' /etc/locale.gen
