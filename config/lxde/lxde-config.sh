9b616d0b63114c1762d98c5c7208a89b17bb4dcf
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-08 11:04:17 -0300

    teste

--- config/lxde/lxde-config.sh
+++ config/lxde/lxde-config.sh
@@ -6,3 +6,11 @@ sudo apt-get update
 sudo apt-get install lxde-core lxterminal --no-install-recommends -y
 sudo apt-get clean
 
+mkdir -p ~/.vnc
+chmod +x /usr/local/bin/startvnc
+chmod +x /usr/local/bin/stopvnc
+
+echo "export DISPLAY=":1"" >> /etc/profile
+source /etc/profile
+
+vncpasswd
