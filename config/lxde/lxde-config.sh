a05d3c871783a59f117b655c6e3aabff86dba418
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-10 23:28:24 -0300

    🖥️  Interface LXDE

--- config/lxde/lxde-config.sh
+++ config/lxde/lxde-config.sh
@@ -3,7 +3,7 @@
 #Get the necessary components
 sudo apt-get update
 
-sudo apt-get install lxde-core lxterminal --no-install-recommends -y
+sudo apt-get install lxde-core lxterminal lxappearance --no-install-recommends -y
 sudo apt-get clean
 
 mkdir -p ~/.vnc
