7f4dccc96c8eff98f5a7e1eb60e107e5f0560efe
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-14 18:28:10 -0300

    🇧🇷 Configurações do idioma português brasileiro

--- config/pt_br/config.sh
+++ config/pt_br/config.sh
@@ -10,25 +10,25 @@ sudo apt-get install locales language-pack-pt -y
 mkdir -p ~/.vnc
 
 if dpkg -l | grep -q lxde-core; then
-echo "#!/bin/bash
-[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-export PULSE_SERVER=127.0.0.1
-LANG=pt_BR.UTF-8
-export LANG
-[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-echo $$ > /tmp/xsession.pid
-dbus-launch --exit-with-session startlxde" > ~/.vnc/xstartup
+    echo "#!/bin/bash
+    [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+    export PULSE_SERVER=127.0.0.1
+    LANG=pt_BR.UTF-8
+    export LANG
+    [ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+    [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+    echo $$ > /tmp/xsession.pid
+    dbus-launch --exit-with-session startlxde" > ~/.vnc/xstartup
 elif dpkg -l | grep -q xfce4; then
-echo "#!/bin/bash
-[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-export PULSE_SERVER=127.0.0.1
-LANG=pt_BR.UTF-8
-export LANG
-[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-echo $$ > /tmp/xsession.pid
-dbus-launch --exit-with-session /usr/bin/startxfce4" > ~/.vnc/xstartup
+    echo "#!/bin/bash
+    [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+    export PULSE_SERVER=127.0.0.1
+    LANG=pt_BR.UTF-8
+    export LANG
+    [ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+    [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+    echo $$ > /tmp/xsession.pid
+    dbus-launch --exit-with-session /usr/bin/startxfce4" > ~/.vnc/xstartup
 else
     echo "null"
 fi
