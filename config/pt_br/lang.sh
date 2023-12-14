7ea3e15a6bdaa91f887a013394153baa5d476a20
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-14 18:28:46 -0300

    🇧🇷 Baixar configuração de idioma

--- config/pt_br/lang.sh
+++ config/pt_br/lang.sh
@@ -4,10 +4,10 @@ wget --tries=20 $extralink/pt_br/tigervnc/vnc -P /usr/local/bin > /dev/null
 wget --tries=20 $extralink/pt_br/tigervnc/vncpasswd -P /usr/local/bin > /dev/null
 wget --tries=20 $extralink/pt_br/tigervnc/stopvnc -P /usr/local/bin > /dev/null
 if dpkg -l | grep -q lxde-core; then
-wget --tries=20  $extralink/pt_br/tigervnc/lxde/startvnc -P /usr/local/bin > /dev/null
+  wget --tries=20  $extralink/pt_br/tigervnc/lxde/startvnc -P /usr/local/bin > /dev/null
 elif dpkg -l | grep -q xfce4; then
-wget --tries=20  $extralink/pt_br/tigervnc/xfce/startvnc -P /usr/local/bin > /dev/null
+  wget --tries=20  $extralink/pt_br/tigervnc/xfce/startvnc -P /usr/local/bin > /dev/null
 else
-echo 'Não identifiquei a interface'
+  echo 'Não identifiquei a interface'
 fi
 clear
