f9858a1610483b96ee2a8a614b690d4d8d60e3e0
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-15 03:32:26 -0300

    🇧🇷 Configurações de idioma

--- config/pt_br/config.sh
+++ config/pt_br/config.sh
@@ -7,35 +7,6 @@ sudo apt-get update
 # Mudar o idioma para o Portuguê Brasileiro [pt_BR]
 sudo apt-get install locales language-pack-pt -y
 
-mkdir -p ~/.vnc
-
-if dpkg -l | grep -q lxde-core; then
-    echo "#!/bin/bash
-    [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-    export PULSE_SERVER=127.0.0.1
-    LANG=pt_BR.UTF-8
-    export LANG
-    [ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-    [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-    echo $$ > /tmp/xsession.pid
-    dbus-launch --exit-with-session startlxde" > ~/.vnc/xstartup
-fi
-
-if dpkg -l | grep -q xfce4; then
-    echo "#!/bin/bash
-    [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-    export PULSE_SERVER=127.0.0.1
-    LANG=pt_BR.UTF-8
-    export LANG
-    [ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-    [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-    echo $$ > /tmp/xsession.pid
-    dbus-launch --exit-with-session /usr/bin/startxfce4" > ~/.vnc/xstartup
-fi
-
-chmod +x ~/.vnc/xstartup
-
-
 ## Gerar o idioma
 sed -i 's/^# *\(pt_BR.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
