025e7c26e2dc89fecc6e3dd37098ec81f01c93cd
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-15 02:49:49 -0300

    🎨 Configurações do tema

--- config/lxde/lxde-themes-config.sh
+++ config/lxde/lxde-themes-config.sh
@@ -2,8 +2,7 @@
 
 extralink="https://raw.githubusercontent.com/allytiago/Ubuntu-no-Android/beta/config"
 
-# Customização do painel xfce
-wget $extralink/lxde/lxpanel.tar.gz
+
 
 vncserver -name remote-desktop -geometry 1920x1080 :1
 
@@ -13,10 +12,13 @@ if [ ! -d "$HOME/.config/lxpanel/" ];then
   mkdir -p "$HOME/.config/lxpanel/"
 fi
 
+# Customização do painel xfce
+
+wget "$extralink/lxde/lxpanel.tar.gz"
 tar -xf lxpanel.tar.xz -C $HOME/.config/ lxpanel
 
 # Definir o papel de parede
-pcmanfm --set-wallpaper /usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg --wallpaper-mode stretch
+#pcmanfm --set-wallpaper /usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg --wallpaper-mode stretch
 
 #Definir tema
 sed -i 's/ThemeName=.*/ThemeName=ZorinBlue-Dark/' ~/.config/lxsession/LXDE/desktop.conf
