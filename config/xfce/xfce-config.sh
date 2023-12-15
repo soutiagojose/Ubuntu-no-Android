adf4a8f47dc653afbef1694189324dd2942c4fa1
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-15 03:38:43 -0300

    🖥️ Interface do XFCE

--- config/xfce/xfce-config.sh
+++ config/xfce/xfce-config.sh
@@ -6,12 +6,37 @@ sudo apt-get update
 sudo apt-get install xfce4 xfce4-goodies xfce4-terminal xfce4-panel-profiles dbus-x11 --no-install-recommends -y
 sudo apt-get clean
 
-mkdir -p ~/.vnc
+
+## Seletor de idiomas
+export USER=$(whoami)
+HEIGHT=0
+WIDTH=0
+CHOICE_HEIGHT=5
+TITLE="Select"
+MENU="Escolha algumas das seguintes opções: \n \nChoose any of the following options: "
+export PORT=1
+
+OPTIONS=(1 "Português (brasileiro)")
+
+CHOICE=$(dialog --clear \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+1)
+wget $extralink/pt_br/config.sh -O locale-config.sh
+wget $extralink/pt_br/tigervnc/xfce/locale.sh
+;;
+esac
+
+
+#mkdir -p ~/.vnc
 chmod +x /usr/local/bin/startvnc
 chmod +x /usr/local/bin/stopvnc
-
-echo "export DISPLAY=":1"" >> /etc/profile
-source /etc/profile
 
 
 vncpasswd
