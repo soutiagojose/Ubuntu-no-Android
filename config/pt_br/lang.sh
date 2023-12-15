b36a6fc4ac448e292e8303bebb670f112845999f
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-15 01:56:37 -0300

    🇧🇷 Baixar configuração de idioma

--- config/pt_br/lang.sh
+++ config/pt_br/lang.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
 extralink="https://raw.githubusercontent.com/allytiago/Ubuntu-no-Android/beta/config"
+# \e[32m greem
 
-echo "Você escolheu o idioma Português Brasileiro"
-wget --tries=20 $extralink/pt_br/config.sh -O $folder/root/lang-config.sh
-wget --tries=20 $extralink/pt_br/tigervnc/vnc -P /usr/local/bin > /dev/null
-wget --tries=20 $extralink/pt_br/tigervnc/vncpasswd -P /usr/local/bin > /dev/null
-wget --tries=20 $extralink/pt_br/tigervnc/stopvnc -P /usr/local/bin > /dev/null
+echo "\e[32mVocê escolheu o idioma Português Brasileiro \e[0m"
 
-if dpkg -s lxde-core >/dev/null 2>&1; then
-    wget --tries=20 "$extralink/pt_br/tigervnc/lxde/startvnc" -P /usr/local/bin > /dev/null
-elif dpkg -s xfce4 >/dev/null 2>&1; then
-    wget --tries=20 "$extralink/pt_br/tigervnc/xfce/startvnc" -P /usr/local/bin > /dev/null
-else
-    echo "Nenhum dos pacotes lxde-core ou xfce4 está instalado."
+if [ ! -d "/usr/local/bin/vnc" ];then
+  wget --tries=20 $extralink/pt_br/tigervnc/vnc -P /usr/local/bin > /dev/null
 fi
+if [ ! -d "/usr/local/bin/vncpasswd" ];then
+  wget --tries=20 $extralink/pt_br/tigervnc/vncpasswd -P /usr/local/bin > /dev/null
+fi
+
+if [ ! -d "/usr/local/bin/stopvnc" ];then
+  wget --tries=20 $extralink/pt_br/tigervnc/stopvnc -P /usr/local/bin > /dev/null
+fi
+
+
+if dpkg -l | grep -q lxde-core; then
+    wget --tries=20 "$extralink/pt_br/tigervnc/lxde/startvnc" -P /usr/local/bin > /dev/null
+fi
+
+if dpkg -l | grep -q xfce4; then
+    wget --tries=20 "$extralink/pt_br/tigervnc/xfce/startvnc" -P /usr/local/bin > /dev/null
+fi
+
+
+
 
