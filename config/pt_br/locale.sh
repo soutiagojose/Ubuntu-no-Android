10c085762caa7a0a6c4e3c0bf2d2a7b5b8328c07
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-15 02:21:05 -0300

    🇧🇷 Baixar configuração de idioma

--- config/pt_br/locale.sh
+++ config/pt_br/locale.sh
@@ -6,15 +6,15 @@ extralink="https://raw.githubusercontent.com/allytiago/Ubuntu-no-Android/beta/co
 echo "\e[32mVocê escolheu o idioma Português Brasileiro \e[0m"
 
 if [ ! -d "/usr/local/bin/vnc" ];then
-  wget --tries=20 $extralink/pt_br/tigervnc/vnc -P /usr/local/bin > /dev/null
+  wget --tries=20 "$extralink/pt_br/tigervnc/vnc" -P /usr/local/bin > /dev/null
 fi
 
 if [ ! -d "/usr/local/bin/vncpasswd" ];then
-  wget --tries=20 $extralink/pt_br/tigervnc/vncpasswd -P /usr/local/bin > /dev/null
+  wget --tries=20 "$extralink/pt_br/tigervnc/vncpasswd" -P /usr/local/bin > /dev/null
 fi
 
 if [ ! -d "/usr/local/bin/stopvnc" ];then
-  wget --tries=20 $extralink/pt_br/tigervnc/stopvnc -P /usr/local/bin > /dev/null
+  wget --tries=20 "$extralink/pt_br/tigervnc/stopvnc" -P /usr/local/bin > /dev/null
 fi
 
 if dpkg -l | grep -q lxde-core;then
