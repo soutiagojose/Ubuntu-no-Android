771ef018a02024f10b16f562d99e9a7bfc638557
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-15 03:31:47 -0300

    🇧🇷 Configurações de idioma

--- config/pt_br/tigervnc/xfce/locale.sh
+++ config/pt_br/tigervnc/xfce/locale.sh
@@ -14,3 +14,18 @@ chmod +x /usr/local/bin/vnc
 chmod +x /usr/local/bin/vncpasswd
 chmod +x /usr/local/bin/startvnc
 chmod +x /usr/local/bin/stopvnc
+
+
+mkdir -p ~/.vnc
+
+echo "#!/bin/bash
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+export PULSE_SERVER=127.0.0.1
+LANG=pt_BR.UTF-8
+export LANG
+[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+echo $$ > /tmp/xsession.pid
+dbus-launch --exit-with-session /usr/bin/startxfce4" > ~/.vnc/xstartup
+
+chmod +x ~/.vnc/xstartup
