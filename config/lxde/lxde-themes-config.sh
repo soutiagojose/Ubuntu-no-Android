43460324ec584d0809626b499e8d9fff6faa2e00
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-15 01:14:00 -0300

    🎨  Configurações do tema

---
+++ config/lxde/lxde-themes-config.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+extralink="https://raw.githubusercontent.com/allytiago/Ubuntu-no-Android/beta/config"
+
+# Customização do painel xfce
+wget $extralink/lxde/lxpanel.tar.gz
+
+vncserver -name remote-desktop -geometry 1920x1080 :1
+
+
+#Configurações do painel
+if [ ! -d "$HOME/.config/lxpanel/" ];then
+  mkdir -p "$HOME/.config/lxpanel/"
+fi
+
+tar -xf lxpanel.tar.xz -C $HOME/.config/ lxpanel
+
+# Definir o papel de parede
+pcmanfm --set-wallpaper /usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg --wallpaper-mode stretch
+
+#Definir tema
+sed -i 's/ThemeName=.*/ThemeName=ZorinBlue-Dark/' ~/.config/lxsession/LXDE/desktop.conf
+
+# Definir pacote de ícones
+sed -i 's/IconThemeName=.*/IconThemeName=Uos-fulldistro-icons/' ~/.config/lxsession/LXDE/desktop.conf
+
+vncserver -kill
+rm -rf /tmp/.X$pt-lock
+rm -rf /tmp/.X11-unix/X$pt
+rm -rf xfce4-panel.tar.bz2
+rm -rf Uos-fulldistro-icons.tar.xz
