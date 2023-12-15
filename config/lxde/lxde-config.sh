883a31473a1cc420751dcb5e33fa78dcbaf4cb8f
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-15 03:35:34 -0300

    🖥️ Interface do LXDE

--- config/lxde/lxde-config.sh
+++ config/lxde/lxde-config.sh
@@ -8,7 +8,34 @@ sudo apt-get update
 sudo apt-get install lxde-core lxterminal lxappearance --no-install-recommends -y
 sudo apt-get clean
 
-mkdir -p ~/.vnc
+## Seletor de idiomas
+export USER=$(whoami)
+HEIGHT=0
+WIDTH=0
+CHOICE_HEIGHT=5
+TITLE="Select"
+MENU="Escolha algumas das seguintes opções: \n \nChoose any of the following options: "
+export PORT=1
+
+OPTIONS=(1 "Português (brasileiro)")
+
+CHOICE=$(dialog --clear \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+1)
+wget $extralink/pt_br/config.sh -O locale-config.sh
+wget $extralink/pt_br/tigervnc/lxde/locale.sh
+;;
+esac
+
+
+#mkdir -p ~/.vnc
 chmod +x /usr/local/bin/startvnc
 chmod +x /usr/local/bin/stopvnc
 
