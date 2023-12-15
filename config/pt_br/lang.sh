9032d3895df656407720a901b59053dc8868675f
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-15 00:46:04 -0300

    🇧🇷 Baixar configuração de idioma

--- config/pt_br/lang.sh
+++ config/pt_br/lang.sh
@@ -1,13 +1,18 @@
+#!/bin/bash
+
+extralink="https://raw.githubusercontent.com/allytiago/Ubuntu-no-Android/beta/config"
+
 echo "Você escolheu o idioma Português Brasileiro"
 wget --tries=20 $extralink/pt_br/config.sh -O $folder/root/lang-config.sh
 wget --tries=20 $extralink/pt_br/tigervnc/vnc -P /usr/local/bin > /dev/null
 wget --tries=20 $extralink/pt_br/tigervnc/vncpasswd -P /usr/local/bin > /dev/null
 wget --tries=20 $extralink/pt_br/tigervnc/stopvnc -P /usr/local/bin > /dev/null
-if dpkg -l | grep -q lxde-core; then
-  wget --tries=20  $extralink/pt_br/tigervnc/lxde/startvnc -P /usr/local/bin > /dev/null
-elif dpkg -l | grep -q xfce4; then
-  wget --tries=20  $extralink/pt_br/tigervnc/xfce/startvnc -P /usr/local/bin > /dev/null
+
+if dpkg -s lxde-core >/dev/null 2>&1; then
+    wget --tries=20 "$extralink/pt_br/tigervnc/lxde/startvnc" -P /usr/local/bin > /dev/null
+elif dpkg -s xfce4 >/dev/null 2>&1; then
+    wget --tries=20 "$extralink/pt_br/tigervnc/xfce/startvnc" -P /usr/local/bin > /dev/null
 else
-  echo 'Não identifiquei a interface'
+    echo "Nenhum dos pacotes lxde-core ou xfce4 está instalado."
 fi
-clear
+
