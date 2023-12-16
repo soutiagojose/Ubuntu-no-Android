668e81ddafd84eafe79da45854494abc4ab92ce9
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-16 18:56:57 -0300

    🎨 Configurações de tema

--- config/xfce/xfce4-themes-config.sh
+++ config/xfce/xfce4-themes-config.sh
@@ -8,8 +8,6 @@ vncserver -name remote-desktop -geometry 1920x1080 :1
 xfconf-query -c xsettings -p /Net/ThemeName -s ZorinBlue-Dark
 xfconf-query -c xsettings -p /Net/IconThemeName -s Uos-fulldistro-icons
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-image -s /usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-image -s /usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg
-xfce4-panel-profiles load xfce4-panel.tar.bz2
 xfce4-panel-profiles load xfce4-panel.tar.bz2
 
 rm -rf xfce4-panel.tar.bz2
