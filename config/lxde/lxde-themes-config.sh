631a19138b40ea6732808a200bfc7c929798bd2d
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-16 00:58:53 -0300

    🎨 Configurações do tema

--- config/lxde/lxde-themes-config.sh
+++ config/lxde/lxde-themes-config.sh
@@ -8,23 +8,26 @@ vncserver -name remote-desktop -geometry 1920x1080 :1
 
 
 #Configurações do painel
-if [ ! -d "$HOME/.config/lxpanel/" ];then
-  mkdir -p "$HOME/.config/lxpanel/"
-fi
+#if [ ! -d "$HOME/.config/lxpanel/" ];then
+#  mkdir -p "$HOME/.config/lxpanel/"
+#fi
 
 # Customização do painel xfce
 
-wget "$extralink/lxde/lxpanel.tar.gz"
+wget "$extralink/lxde/lxpanel.tar.xz"
+wget "$extralink/lxde/lxsession.tar.xz"
 tar -xf lxpanel.tar.xz -C $HOME/.config/ lxpanel
+tar -xf lxsession.tar.xz -C $HOME/.config/ lxpanel
 
 # Definir o papel de parede
 #pcmanfm --set-wallpaper /usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg --wallpaper-mode stretch
+sed -i 's|wallpaper=/etc/alternatives/desktop-background|wallpaper=/usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg|' ~/.config/pcmanfm/LXDE/desktop-items-0.conf
 
 #Definir tema
-sed -i 's/ThemeName=.*/ThemeName=ZorinBlue-Dark/' ~/.config/lxsession/LXDE/desktop.conf
+#sed -i 's|sNet/ThemeName=*|sNet/ThemeName=ZorinBlue-Dark|' ~/.config/lxsession/LXDE/desktop.conf
 
 # Definir pacote de ícones
-sed -i 's/IconThemeName=.*/IconThemeName=Uos-fulldistro-icons/' ~/.config/lxsession/LXDE/desktop.conf
+#sed -i 's|sNet/IconThemeName=*|sNet/IconThemeName=Uos-fulldistro-icons/' ~/.config/lxsession/LXDE/desktop.conf
 
 vncserver -kill
 rm -rf /tmp/.X$pt-lock
