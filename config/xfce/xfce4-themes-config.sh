f6abaedb56130a8b0231ca54cf92ee6d3323389c
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-16 22:40:59 -0300

    Update xfce4-themes-config.sh

--- config/xfce/xfce4-themes-config.sh
+++ config/xfce/xfce4-themes-config.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-export USER=$(whoami)
-export PORT=1
 extralink="https://raw.githubusercontent.com/allytiago/Ubuntu-no-Android/beta/config"
 #
 # Customização do painel xfce
 wget $extralink/xfce/xfce4-panel.tar.bz2
-
-GEO="" vnc
-vncserver -kill
-rm -rf /root/.vnc/localhost:5901.pid
-rm -rf /tmp/.X$pt-lock
-rm -rf /tmp/.X11-unix/X$pt
 
 vncserver -name remote-desktop -geometry 1920x1080 :1
 echo -e ""
@@ -54,13 +46,6 @@ vncserver -kill
 rm -rf /root/.vnc/localhost:5901.pid
 rm -rf /tmp/.X$pt-lock
 rm -rf /tmp/.X11-unix/X$pt
-
-vncserver -name remote-desktop -geometry 1920x1080 :1
-vncserver -kill
-rm -rf /root/.vnc/localhost:5901.pid
-rm -rf /tmp/.X$pt-lock
-rm -rf /tmp/.X11-unix/X$pt
-
 
 vncserver -name remote-desktop -geometry 1920x1080 :1
 xfconf-query -c xsettings -p /Net/ThemeName -s ZorinBlue-Dark
