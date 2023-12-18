1b5c901428b8c20250fccc91c8f8a12ee48f4621
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-17 22:39:53 -0300

    Update xfce4-themes-config.sh

--- config/xfce/xfce4-themes-config.sh
+++ config/xfce/xfce4-themes-config.sh
@@ -31,71 +31,22 @@ echo -e "
 
 
 
-
-
-
-
-
-
-
-
 "
-xfconf-query -c xsettings -p /Net/ThemeName -s ZorinBlue-Dark
-
-vncserver -kill
-rm -rf /root/.vnc/localhost:5901.pid
-rm -rf /tmp/.X$pt-lock
-rm -rf /tmp/.X11-unix/X$pt
-
-vncserver -name remote-desktop -geometry 1920x1080 :1
-mkdir $HOME/.config/xfce4/
-mkdir $HOME/.config/xfce4/xconf
-mkdir $HOME/.config/xfce4/xconf/xfce-perchannel-xml
-
 xfconf-query -c xsettings -p /Net/ThemeName -s ZorinBlue-Dark
 xfconf-query -c xsettings -p /Net/IconThemeName -s Uos-fulldistro-icons
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-image -s /usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg
 xfce4-panel-profiles load xfce4-panel.tar.bz2
 
-sed -i 's|property name="last-image" type="string" value="/usr/share/backgrounds/xfce/xfce-verticals.png"property name="last-image" type="string" value="/usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg"|' $HOME/.config/xfce4/xconf/xfce-perchannel-xml/xfce4-desktop.xml
+#mkdir $HOME/.config/xfce4/
+#mkdir $HOME/.config/xfce4/xconf
+#mkdir $HOME/.config/xfce4/xconf/xfce-perchannel-xml
 
-
-echo -e "
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"
-
-rm -rf xfce4-panel.tar.bz2
-rm -rf Uos-fulldistro-icons.tar.xz
+#sed -i 's|property name="last-image" type="string" value="/usr/share/backgrounds/xfce/xfce-verticals.png"property name="last-image" type="string" value="/usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg"|' $HOME/.config/xfce4/xconf/xfce-perchannel-xml/xfce4-desktop.xml
 
 vncserver -kill
 rm -rf /root/.vnc/localhost:5901.pid
 rm -rf /tmp/.X$pt-lock
 rm -rf /tmp/.X11-unix/X$pt
-
-# Replicação para casos de erro
+rm -rf xfce4-panel.tar.bz2
+rm -rf Uos-fulldistro-icons.tar.xz
 
