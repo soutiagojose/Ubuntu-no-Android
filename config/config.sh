3517b85f263014ae76e9e22c3d20db62ae033ef2
AllyTiago <47113005+allytiago@users.noreply.github.com>
2024-01-29 18:09:24 -0300

    ⚙️ Configurações principais do sistema

--- config/config.sh
+++ config/config.sh
@@ -16,3 +16,20 @@ sudo apt-get install dbus-x11 -y
 sudo apt-get install exo-utils --no-install-recommends -y
 sudo apt get install tigervnc-standalone-server tigervnc-common tigervnc-tools --no-install-recommends -y
 
+
+# Baixando papel de parede
+if [ ! -d "/usr/share/backgrounds/" ];then
+  mkdir -p "/usr/share/backgrounds/"
+fi
+
+wget $extralink/wallpapers/unsplash/john-towner-JgOeRuGD_Y4.jpg  -O /usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg
+
+# Baixando pacote de icones
+if [ ! -d "/usr/share/icons/" ];then
+  mkdir -p "/usr/share/icons/"
+fi
+
+wget $extralink/icons/Uos-fulldistro-icons.tar.xz
+
+
+tar -xf Uos-fulldistro-icons.tar.xz -C /usr/share/icons
