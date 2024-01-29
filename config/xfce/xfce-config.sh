678674509a2fdf9794a46dfbb9de48f33acbacd0
AllyTiago <47113005+allytiago@users.noreply.github.com>
2024-01-29 18:39:40 -0300

    🖥️ Interface do XFCE

--- config/xfce/xfce-config.sh
+++ config/xfce/xfce-config.sh
@@ -2,23 +2,29 @@
 
 extralink="https://raw.githubusercontent.com/allytiago/Ubuntu-no-Android/main/config"
 
-#Get the necessary components
-sudo apt-get update
+wget --tries=20 $extralink/config.sh -O $folder/root/config.sh
+chmod +x config.sh
 
-sudo apt-get install xfce4 xfce4-goodies xfce4-terminal xfce4-panel-profiles dbus-x11 --no-install-recommends -y
+bash config.sh
+
+# Instalar pacotes necessários da interface
+sudo apt-get install lxde-core lxterminal lxappearance --no-install-recommends -y
 sudo apt-get clean
 
+# Pasta resposável pela execução do vnc
 mkdir -p ~/.vnc
 
-echo '#!/bin/bash
+echo "#!/bin/bash
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
 export PULSE_SERVER=127.0.0.1
 export LANG
 [ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
 [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
 echo $$ > /tmp/xsession.pid
-dbus-launch --exit-with-session /usr/bin/startxfce4' > ~/.vnc/xstartup
+dbus-launch --exit-with-session /usr/bin/startxfce4" > ~/.vnc/xstartup
 
 chmod +x ~/.vnc/xstartup
+
 
 ## Seletor de idiomas
 export USER=$(whoami)
@@ -41,27 +47,68 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
 1)
-wget --tries=20 $extralink/pt_br/tigervnc/xfce/locale.sh
-wget --tries=20 $extralink/pt_br/config.sh -O locale-config.sh
+echo -e  "\033[0;32mVocê escolheu o idioma Português Brasileiro\033[0m"
+echo "As configurações de idioma já serão instaladas..."
+
+# Mudar o idioma para o Português Brasileiro [pt_BR]
+sudo apt-get install locales language-pack-pt language-pack-pt-base language-pack-gnome-pt language-pack-gnome-pt-base -y
+
+## Gerar o idioma
+sed -i 's/^# *\(pt_BR.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+## Exportar os comandos de configuração de idioma para ~/.bashrc
+## Essa configuração necessita de reboot
+echo 'export LC_ALL=pt_BR.UTF-8' >> ~/.bashrc
+echo 'export LANG=pt_BR.UTF-8' >> ~/.bashrc
+echo 'export LANGUAGE=pt_BR.UTF-8' >> ~/.bashrc
+
+# Arquivos necessários para a tradução do VNC
+wget --tries=20 "$extralink/pt_br/tigervnc/vnc" -P /usr/local/bin > /dev/null
+wget --tries=20 "$extralink/pt_br/tigervnc/vncpasswd" -P /usr/local/bin > /dev/null
+wget --tries=20 "$extralink/pt_br/tigervnc/stopvnc" -P /usr/local/bin > /dev/null
+wget --tries=20 "$extralink/pt_br/tigervnc/xfce/startvnc" -P /usr/local/bin > /dev/null
 wget --tries=20 $extralink/pt_br/tigervnc/xfce/startvncserver -P /usr/local/bin > /dev/null
-sed -i '\|export PULSE_SERVER=127.0.0.1|a LANG=pt_BR.UTF-8|' ~/.vnc/xstartup
-bash locale.sh
-bash locale-config.sh
+
+sed -i 's|export LANG|LANG=pt_BR.UTF-8\nexport LANG|' ~/.vnc/xstartup
 ;;
 esac
 
 chmod +x /usr/local/bin/startvnc
-chmod +x /usr/local/bin/stopvnc
 chmod +x /usr/local/bin/startvncserver
+chmod +x /usr/local/bin/stopvnc
+chmod +x /usr/local/bin/vnc
+chmod +x /usr/local/bin/vncpasswd
+
 
 echo "export DISPLAY=":1"" >> /etc/profile
 source /etc/profile
 
 vncpasswd
 
-wget --tries=20 $extralink/xfce/xfce4-themes-config.sh
-chmod +x  xfce4-themes-config.sh
-bash xfce4-themes-config.sh
+# Configuração do tema
+vncserver -name remote-desktop -geometry 1920x1080 :1
 
-rm -rf locale.sh
-rm -rf locale-config.sh
+mkdir $HOME/.config/gtk-3.0/
+
+#Adicionar o atalho da memória interna do aparelho nos gerenciadores de arquivos
+echo 'file:///sdcard' | sudo tee $HOME/.config/gtk-3.0/bookmarks
+
+## Tema dark
+xfconf-query -c xsettings -p /Net/ThemeName -s ZorinBlue-Dark
+# Pacote de icones
+xfconf-query -c xsettings -p /Net/IconThemeName -s Uos-fulldistro-icons
+## papel de parede
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-image -s /usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg
+
+# Estilo do painel
+wget --tries=20 $extralink/xfce/xfce4-panel.tar.bz2 $folder/root/xfce4-panel.tar.bz2
+chmod +x ./xfce4-panel.tar.bz2
+xfce4-panel-profiles load xfce4-panel.tar.bz2
+
+vncserver -kill
+rm -rf /root/.vnc/localhost:5901.pid
+rm -rf /tmp/.X$pt-lock
+rm -rf /tmp/.X11-unix/X$pt
+rm -rf xfce4-panel.tar.bz2
+rm -rf Uos-fulldistro-icons.tar.xz
