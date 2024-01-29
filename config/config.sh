4438d544bb07fc7c511e028204616e2ebf3c1255
AllyTiago <47113005+allytiago@users.noreply.github.com>
2024-01-29 20:36:39 -0300

    ⚙️ Configurações principais do sistema

--- config/config.sh
+++ config/config.sh
@@ -15,8 +15,6 @@ sudo apt-get install inetutils-tools -y
 sudo apt-get install dbus-x11 -y
 sudo apt-get install exo-utils --no-install-recommends -y
 sudo apt get install tigervnc-standalone-server tigervnc-common tigervnc-tools --no-install-recommends -y
-sudo apt-get install keyboard-configuration -y
-sudo apt-get install tzdata -y
 #sudo apt install udisks2 -y
 #echo "" > /var/lib/dpkg/info/udisks2.postinst
 #sudo dpkg --configure -a
