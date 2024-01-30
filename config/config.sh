792f6a79dfb74183078c659cb4d70668fdb956b9
AllyTiago <47113005+allytiago@users.noreply.github.com>
2024-01-30 00:30:10 -0300

    ⚙️ Configurações principais do sistema

--- config/config.sh
+++ config/config.sh
@@ -14,7 +14,7 @@ sudo apt-get install dialog -y
 sudo apt-get install inetutils-tools -y
 sudo apt-get install dbus-x11 -y
 sudo apt-get install exo-utils --no-install-recommends -y
-sudo apt get install tigervnc-standalone-server tigervnc-common tigervnc-tools --no-install-recommends -y
+sudo apt-get install tigervnc-standalone-server tigervnc-common tigervnc-tools --no-install-recommends -y
 #sudo apt install udisks2 -y
 #echo "" > /var/lib/dpkg/info/udisks2.postinst
 #sudo dpkg --configure -a
@@ -25,15 +25,13 @@ if [ ! -d "/usr/share/backgrounds/" ];then
   mkdir -p "/usr/share/backgrounds/"
 fi
 
-wget "$extralink/wallpapers/unsplash/john-towner-JgOeRuGD_Y4.jpg"  -O /usr/share/backgrounds/john-towner-JgOeRuGD_Y4.jpg
+wget --tries=20 "$extralink/wallpapers/unsplash/john-towner-JgOeRuGD_Y4.jpg" -P /usr/share/backgrounds
 
 # Baixando pacote de icones
 if [ ! -d "/usr/share/icons" ];then
   mkdir -p "/usr/share/icons"
 fi
 
-
-
-wget "$extralink/icons/Uos-fulldistro-icons.tar.xz"
+wget --tries=20 "$extralink/icons/Uos-fulldistro-icons.tar.xz" -P /usr/share/icons
 
 tar -xf Uos-fulldistro-icons.tar.xz -C /usr/share/icons
