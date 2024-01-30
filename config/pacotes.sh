0e8e24c9523e9612a203fdee6ec77819ad490771
AllyTiago <47113005+allytiago@users.noreply.github.com>
2024-01-30 16:13:10 -0300

    testes

--- config/pacotes.sh
+++ config/pacotes.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 sudo apt-get install software-properties-common --no-install-recommends -y
-sudo apt-get install nautilus -y
-sudo apt-get install neofetch -y
-sudo apt-get install gdebi -y
-sudo apt-get install font-vewer -y
-sudo apt-get install tumbler -y
-sudo apt-get install gpg curl -y
-sudo apt-get install bleachbit -y
-sudo apt-get install keyboard-configuration -y
-sudo apt-get install tzdata -y #dpkg-reconfigure tzdata
 
 # PPAs de repositórios
 
@@ -29,12 +20,14 @@ sudo add-apt-repository ppa:zorinos/stable -y
 sudo add-apt-repository ppa:chromium-team/beta -y
 
 ## PPA do VSCode
+sudo apt-get install gpg -y
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 
 ## PPA do Brave Browser
+sudo apt-get install curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
@@ -72,7 +65,10 @@ sudo apt update
 
 # Instalar os pacotes
 sudo apt install zorin-desktop-themes -y
-sudo apt install firefox chromium-browser apt-transport-https code -y
+sudo apt install apt-transport-https -y
+sudo apt update
+sudo apt install code -y
+sudo apt install chromium-browser -y
 
 # Permitir a inicialização
 # Para o VSCode
@@ -80,3 +76,12 @@ sed -i 's|Exec=/usr/share/code/code|Exec=/usr/share/code/code --no-sandbox|' /us
 
 # Para Chromium Web Browser
 sed -i 's|Exec=chromium-browser|Exec=chromium-browser --no-sandbox|' /usr/share/applications/chromium-browser.desktop
+
+sudo apt-get install nautilus -y
+sudo apt-get install neofetch -y
+sudo apt-get install gdebi -y
+sudo apt-get install font-vewer -y
+sudo apt-get install tumbler -y
+sudo apt-get install bleachbit -y
+sudo apt-get install keyboard-configuration -y
+sudo apt-get install tzdata -y #dpkg-reconfigure tzdata
