d2753ed2aa02f825f6b77a0e98d629107cc7c8a9
AllyTiago <47113005+allytiago@users.noreply.github.com>
2024-02-02 17:54:21 -0300

    🚀 Instalador do Ubuntu

--- install.sh
+++ install.sh
@@ -263,9 +263,8 @@ echo -e  "\033[0;32mVocê escolheu o idioma Português Brasileiro\033[0m"
 echo "As configurações de idioma já serão instaladas..."
 echo -e  "\033[0;32mVocê escolheu o idioma Português Brasileiro\033[0m"
 echo "As configurações de idioma já serão instaladas..."
-wget --tries=20 "$extralink/pt_br/language-pt_br.sh" -O $folder/root/language-base.sh
+wget --tries=20 "$extralink/pt_br/language-base.sh" -O $folder/root/language-base.sh
 chmod +x $folder/root/language-base.sh
-
 ;;
 esac
 
