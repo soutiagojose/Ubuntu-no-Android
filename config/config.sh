3d572a2243042d657737491e03bc9f544330c3dc
AllyTiago <47113005+allytiago@users.noreply.github.com>
2024-02-02 18:45:27 -0300

    ⚙️ Configurações principais do sistema

--- config/config.sh
+++ config/config.sh
@@ -7,12 +7,7 @@ sudo apt-get update
 sudo apt-get full-upgrade -y
 
 # Instalar pacotes necessários
-sudo apt-get install sudo -y
-sudo apt-get install wget -y
-sudo apt-get install nano -y
-sudo apt-get install dialog -y
-sudo apt-get install inetutils-tools -y
-sudo apt-get install dbus-x11 -y
+sudo apt-get install sudo wget nano dialog inetutils-tools dbus-x11 -y
 sudo apt-get install exo-utils --no-install-recommends -y
 sudo apt-get install tigervnc-standalone-server tigervnc-common tigervnc-tools --no-install-recommends -y
 #sudo apt install udisks2 -y
