f13da02c07314ab0395b6279b4112ff71891c584
AllyTiago <47113005+allytiago@users.noreply.github.com>
2024-02-02 18:08:59 -0300

    🚀 Instalador do Ubuntu

--- install.sh
+++ install.sh
@@ -330,7 +330,7 @@ chmod +x $folder/root/ui-config.sh
 sed -i '\|command+=" /bin/bash --login"|a command+=" -b /data/data/com.termux/files/home/ubuntu22-fs/usr/local/bin/startvncserver"' ./start-ubuntu.sh
 mkdir /data/data/com.termux/files/usr/var/run/dbus
 sed -i '\|unset LD_PRELOAD|a dbus-daemon --fork --config-file=/data/data/com.termux/files/usr/share/dbus-1/system.conf --address=unix:path=system_bus_socket' ./start-ubuntu.sh
-#dbus-daemon --fork --config-file=/data/data/com.termux/files/usr/share/dbus-1/system.conf --address=unix:path=system_bus_socket
+dbus-daemon --fork --config-file=/data/data/com.termux/files/usr/share/dbus-1/system.conf --address=unix:path=system_bus_socket
 sed -i '\|command+=" -b /proc/self/fd:/dev/fd"|a command+=" -b system_bus_socket:/run/dbus/system_bus_socket"' ./start-ubuntu.sh
 ;;
 
