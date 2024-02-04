941c0260fefa20c5232e346c78b76fc3d0a005d9
AllyTiago <47113005+allytiago@users.noreply.github.com>
2024-02-03 23:14:56 -0300

    🖥️ Interface do Gnome

--- config/gnome/gnome-config.sh
+++ config/gnome/gnome-config.sh
@@ -3,33 +3,16 @@
 extralink="https://raw.githubusercontent.com/allytiago/Ubuntu-no-Android/beta/config"
 
 wget --tries=20 "https://raw.githubusercontent.com/allytiago/Ubuntu-no-Android/beta/config/config.sh"
-wget --tries=20 "https://raw.githubusercontent.com/allytiago/Ubuntu-no-Android/beta/config/pacotes.sh"
+#wget --tries=20 "https://raw.githubusercontent.com/allytiago/Ubuntu-no-Android/beta/config/pacotes.sh"
 
 chmod +x config.sh
-chmod +x pacotes.sh
+#chmod +x pacotes.sh
 
 bash config.sh
 
 clear
 echo -e  "\033[0;32mGnome UI\033[0m"
-echo -e "
 
-
-
-
-"
-echo -e "
-
-
-
-
-"
-echo -e "
-
-
-
-
-"
 clear
 # Instalar pacotes necessários da interface
 sudo apt-get install gnome-shell gnome-terminal gnome-tweaks gnome-shell-extensions gnome-shell-extension-ubuntu-dock -y
@@ -42,7 +25,7 @@ echo "#!/bin/bash
 export PULSE_SERVER=127.0.0.1
 export LANG
 export XDG_CURRENT_DESKTOP="GNOME"
-service dbus start
+#service dbus start
 gnome-shell --x11" > ~/.vnc/xstartup
 
 chmod +x ~/.vnc/xstartup
