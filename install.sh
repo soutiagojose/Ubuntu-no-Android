2abedc95fb05493a555b28875d3e8b3dafbe01ab
AllyTiago <47113005+allytiago@users.noreply.github.com>
2024-02-12 20:17:35 -0300

    teste7

--- install.sh
+++ install.sh
@@ -397,3 +397,4 @@ clear" > $folder/root/.bash_profile
 
 bash $bin
 #sed -i '1 a\if [ ! -e "system_bus_socket" ]; then\n	rm -rf /data/data/com.termux/files/usr/var/run/dbus/pid \n	dbus-daemon --fork --config-file=/data/data/com.termux/files/usr/share/dbus-1/system.conf --address=unix:path=system_bus_socket\nfi' $bin
+bash $bin
